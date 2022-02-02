4d6ebf8a8ec30256f27bfdad25a176c98b3baf82
Israel-Ferreira <israelfsouza@hotmail.com>
2022-02-02 17:37:32 -0300

    Enum GraphQl

--- schema-query/projeto/data/db.js
+++ schema-query/projeto/data/db.js
@@ -1,7 +1,7 @@
 const usuarios = [
-    { id: 1, nome: "Israel Souza", email: "israel@example.com", salario: 4000, idade: 22, perfilId: 1 },
-    { id: 2, nome: "Matheus Faria", email: "matheusf@example.com", salario: 5000, idade: 25, perfilId: 2 },
-    { id: 3, nome: "Gileade Trindade", email: "gileadet@example.com", salario: 6500, idade: 30, perfilId: 1 }
+    { id: 1, nome: "Israel Souza", email: "israel@example.com", salario: 4000, idade: 22, perfilId: 1, status: "BLOQUEADO" },
+    { id: 2, nome: "Matheus Faria", email: "matheusf@example.com", salario: 5000, idade: 25, perfilId: 2, status: "ATIVO"  },
+    { id: 3, nome: "Gileade Trindade", email: "gileadet@example.com", salario: 6500, idade: 30, perfilId: 1, status: "INATIVO" }
 ]
 
 
