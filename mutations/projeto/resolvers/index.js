0e4e2c7ae02f6bdbb53953ba65d4ef27b30bab83
israel.souza <israel.souza@summit-bra.com>
2022-04-11 15:18:28 -0300

    Remoção do Resolver de produtos

--- mutations/projeto/resolvers/index.js
+++ mutations/projeto/resolvers/index.js
@@ -1,11 +1,8 @@
-const produtoResolver = require("./produto.resolver")
 const queryResolver = require('./query.resolver')
 const usuarioResolver = require('./usuario.resolver')
 
 
-
 module.exports = {
-    Produto: produtoResolver,
     Usuario: usuarioResolver,
     Query: queryResolver,
 }
--- mutations/projeto/resolvers/produto.resolver.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    precoComDesconto(obj) {
-        if (obj.desconto) {
-            return obj.preco - (obj.preco * obj.desconto)
-        }
-
-        return obj.preco
-    }
-}
