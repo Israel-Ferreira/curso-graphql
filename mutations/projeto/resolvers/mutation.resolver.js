f97709b8b3316e1e73e654e6e7f33600986f5d03
israel.souza <israel.souza@summit-bra.com>
2022-04-11 16:50:53 -0300

    Mutation: Exclusão

--- mutations/projeto/resolvers/mutation.resolver.js
+++ mutations/projeto/resolvers/mutation.resolver.js
@@ -2,6 +2,14 @@ const {usuarios, proximoId, perfis} = require('../data/db')
 
 module.exports = {
     novoUsuario(_, {nome, email, idade}) {
+
+        const emailIsUsed =  usuarios.some(user => user.email === email)
+
+
+        if(emailIsUsed) {
+            throw new Error("E-mail já existente")
+        }
+
         const user = {
             id: proximoId(),
             nome,
@@ -13,5 +21,17 @@ module.exports = {
         usuarios.push(user)
 
         return user
+    },
+    
+    excluirUsuario(_, {id}){
+        const index =  usuarios.findIndex(user => user.id === id)
+
+        if(index === -1){
+            throw new Error("Erro: Usuário não encontrado")
+        }
+        
+        const excluido = usuarios.splice(index, 1)
+        
+        return excluido ? excluido[0] : null
     }
 }
